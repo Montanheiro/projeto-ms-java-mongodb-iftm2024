7b7c356e842add3bb44d819e1cd700d44a43c2d6
Lucas Montanheiro <lucasmontanheiro10@gmail.com>
2024-11-18 17:05:30 -0300

    Update Main.java

--- 03-relacionamentos-mongodb/relacionamentos/src/main/java/com/montanha/mongodb/relacionamentos/Main.java
+++ 03-relacionamentos-mongodb/relacionamentos/src/main/java/com/montanha/mongodb/relacionamentos/Main.java
@@ -1,7 +1,13 @@
 package com.montanha.mongodb.relacionamentos;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+@EnableMongoRepositories
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class, args);
     }
 }
